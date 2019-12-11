597033aabefe0614e98b839fbe4efe29e7242758
Jessica Cristina <37486644+DevJessica@users.noreply.github.com>
2019-12-10 23:42:38 -0300

    Exercicio1 - Aula variavel

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ola mundo");
-		System.out.println("Boa Noite Marido! EU TE AMO!");
-
+		int y = 32;
+		double e = 23.45678;
+		double x = 10.23435;
+		String nome = "Jessica";
+		int idade = 25;
+		double renda = 3500.0;
+		
+		//System.out.println("Ola mundo");
+		//System.out.println("Boa Noite Marido! EU TE AMO!");
+		//System.out.println(y);
+		//System.out.printf("%.2f%n", x);
+		//System.out.println("Resultado " + "=" + y);
+		//System.out.printf("Resultado = %.3f\n %.2f\n" , x,e);
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n" , nome, idade,renda);
 	}
 
 }
